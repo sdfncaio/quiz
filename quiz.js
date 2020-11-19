d805efb7cd55d6f3186ed953279b899e23b9d991
sdfncaio <71291506+sdfncaio@users.noreply.github.com>
2020-11-19 20:57:37 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,119 @@
+/*questao 1*/
+function acertou() {
+    document.getElementById('d1').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d1').style.display="none"
+},2000)
+
+function errou1() {
+    document.getElementById('a1').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('a1').style.display="none";
+},2000)
+
+function errou() {
+    document.getElementById('b1').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('b1').style.display="none";
+},2000)
+
+function errou() {
+    document.getElementById('c1').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('c1').style.display="none";
+},2000)
+
+/*questao 2 */
+
+function acertou() {
+    document.getElementById('d2').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('d2').style.display="none"
+},2000)
+
+function errou1() {
+    document.getElementById('a2').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('a2').style.display="none";
+},2000)
+
+function errou() {
+    document.getElementById('b2').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('b2').style.display="none"
+},2000)
+
+function errou() {
+    document.getElementById('c2').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c2').style.display="none"
+},2000)
+
+/*questao 3 */
+
+function errou1() {
+    document.getElementById('d3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d3').style.display="none"
+},2000)
+
+function acertou() {
+    document.getElementById('a3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a3').style.display="none"
+},2000)
+
+function errou() {
+    document.getElementById('b3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b3').style.display="none"
+},2000)
+
+function errou1() {
+    document.getElementById('c3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c3').style.display="none"
+},2000)
+
+/*questao 4 */
+
+function errou() {
+    document.getElementById('d4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d4').style.display="none"
+},2000)
+
+function errou1() {
+    document.getElementById('a4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a4').style.display="none"
+},2000)
+
+function errou1() {
+    document.getElementById('b4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b4').style.display="none"
+},2000)
+
+function acertou() {
+    document.getElementById('c4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c4').style.display="none"
+},2000) 
+
